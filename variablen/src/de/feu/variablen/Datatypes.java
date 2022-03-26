beea311d1f6c887b263d9f37bb54e625d4f3121c
MichaelZett <michael2.zoeller@gmail.com>
2022-03-26 12:32:34 +0100

    Show datatypes

--- variablen/src/de/feu/variablen/Datatypes.java
+++ variablen/src/de/feu/variablen/Datatypes.java
@@ -3,8 +3,44 @@ package de.feu.variablen;
 public class Datatypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Ganzzahlen
+		short kleineZahl = 1;
+		int ganzzahl = 4;
+		int negativeZahl = -42;
+		long grosseGanzzahl = 2_500_000_000L;
+//		int ichKannAuchGrosseZahlen = 2_500_000_000; geht nicht
 
+		ganzzahl = (int) grosseGanzzahl; // Geht, aber falsch
+		grosseGanzzahl = negativeZahl; // int Teilmenge von Long
+
+		System.out.println("Nach Cast: " + ganzzahl);
+
+		System.out.println("Short max: " + Short.MAX_VALUE);
+		System.out.println("Integer max: " + Integer.MAX_VALUE);
+		System.out.println("Long max: " + Long.MAX_VALUE);
+
+		Long grosseGanzzahlObject = grosseGanzzahl;
+		long zahl = 0;
+		Long zahlWrapper = null;
+		System.out.println("Zahl: " + zahl + " groﬂ Long: " + zahlWrapper);
+
+		// Dezimalzahlen
+		float fliesspunktZahl = 3.5F;
+		double grosseFliesspunktZahl = 2.500_000_000_000_000;
+		System.out.println("Float max: " + Float.MAX_VALUE);
+		System.out.println("Double max: " + Double.MAX_VALUE);
+
+		// Logik
+		boolean logik = true;
+
+		// Buchstaben
+		char buchstabe = 'c';
+
+		String zahlAlsText = String.valueOf(negativeZahl);
+		System.out.println("Text : " + zahlAlsText);
+
+		int neueZahl = Integer.parseInt(zahlAlsText);
+		System.out.println("Wieder Zahl : " + zahlAlsText);
 	}
 
 }
